e837d9d24056c0ed1b9f09cf336a23ee4ecfcadc
gortiz <ggortiz09@gmail.com>
2021-09-14 23:56:20 -0300

    create class contacto

---
+++ src/main/java/com/gortiz/swaggerapi/entity/Contacto.java
@@ -0,0 +1,15 @@
+package com.gortiz.swaggerapi.entity;
+
+import java.io.Serializable;
+
+public class Contacto implements Serializable {
+
+	private Long id;
+	private String nombre;
+	private String telefono;
+	private String email;
+	private String direccion;
+	private String codigopostal;
+	private String nota;
+
+}
