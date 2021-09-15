7f9a1fab28e3f6c573ef41cf022bde5b859eb94f
gortiz <ggortiz09@gmail.com>
2021-09-15 00:19:06 -0300

    update entity with valid

--- src/main/java/com/gortiz/swaggerapi/entity/Contacto.java
+++ src/main/java/com/gortiz/swaggerapi/entity/Contacto.java
@@ -2,14 +2,79 @@ package com.gortiz.swaggerapi.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
 public class Contacto implements Serializable {
 
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
 	private String nombre;
 	private String telefono;
 	private String email;
 	private String direccion;
 	private String codigopostal;
+	@Column(length = 4100)
 	private String nota;
+	
+	public Contacto() {
+		
+	}
+	
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCodigopostal() {
+		return codigopostal;
+	}
+	public void setCodigopostal(String codigopostal) {
+		this.codigopostal = codigopostal;
+	}
+	public String getNota() {
+		return nota;
+	}
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
+	
+	
 
 }
