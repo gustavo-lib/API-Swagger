747f12f4db73ab32126010a87f4561313cb0555a
gortiz <ggortiz09@gmail.com>
2021-09-14 23:42:38 -0300

    first commit

---
+++ src/test/java/com/gortiz/swaggerapi/SwaggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gortiz.swaggerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
