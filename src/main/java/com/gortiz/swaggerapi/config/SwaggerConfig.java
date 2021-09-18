7414ee09e24cce91fec753b9d0f036713be79180
gortiz <ggortiz09@gmail.com>
2021-09-17 23:56:47 -0300

    update

--- src/main/java/com/gortiz/swaggerapi/config/SwaggerConfig.java
+++ src/main/java/com/gortiz/swaggerapi/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.gortiz.swaggerapi.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.Contact;
 
 @Configuration
 @EnableSwagger2
@@ -17,10 +21,26 @@ public class SwaggerConfig {
     public Docket api() { 
 
         return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+        		.select()
+    			//	.apis(RequestHandlerSelectors.any())
+    				.apis(RequestHandlerSelectors.basePackage("com.gortiz.swaggerapi.controller"))
+    				.paths(PathSelectors.any())
+    				.build()
+    				.apiInfo(getApiInfo());                                  
     }
 
+	
+    private ApiInfo getApiInfo() {
+
+        return new ApiInfo(
+                " API Contactos",
+                "Simple ejemplo uso de + Swagger 2",
+                "V1",
+                "",
+                new Contact("Gustavo Ortiz", "https://www.-.-.com", "gortiz@....com"),
+                "CC BY-SA 3.0",
+                "",
+                Collections.emptyList()
+
+        );
 }
