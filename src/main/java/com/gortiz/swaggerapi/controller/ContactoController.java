79c7a97a51c9c90f65b51e1cd8f06b0a6436d3d0
gus <ggortiz09@gmail.com>
2021-09-19 20:47:02 -0300

    new annotation

--- src/main/java/com/gortiz/swaggerapi/controller/ContactoController.java
+++ src/main/java/com/gortiz/swaggerapi/controller/ContactoController.java
@@ -2,6 +2,10 @@ package com.gortiz.swaggerapi.controller;
 
 import java.util.List;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +18,16 @@ import com.gortiz.swaggerapi.entity.Contacto;
 
 @RestController
 @RequestMapping("/api")
+@Api(value = "Swagger API- Gustavo Ortiz", description = "Shows hello world")
 public class ContactoController {
 
+	@ApiOperation(value = "Returns List contactos")
+    @ApiResponses(
+			value = {
+					@ApiResponse(code = 100, message = "100 is the message"),
+					@ApiResponse(code = 200, message = "Successful return list")
+			}
+	)
 	@GetMapping("/contactos")
 	public ResponseEntity<List<Contacto>> findAll() {
 		return null;
