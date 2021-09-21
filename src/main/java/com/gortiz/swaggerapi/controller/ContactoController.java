f28c136d01a331b42c26d22f4b275019fddbd8a9
gortiz <ggortiz09@gmail.com>
2021-09-21 18:23:30 -0300

    api-operation-response swagger

--- src/main/java/com/gortiz/swaggerapi/controller/ContactoController.java
+++ src/main/java/com/gortiz/swaggerapi/controller/ContactoController.java
@@ -12,16 +12,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gortiz.swaggerapi.entity.Contacto;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
 @RestController
 @RequestMapping("/api")
 public class ContactoController {
 
+	 @ApiOperation(value = "Lista de todos los contactos", response = Iterable.class, tags = "getList")
+	    @ApiResponses(value = { 
+	            @ApiResponse(code = 200, message = "Suceess|OK"),
+	            @ApiResponse(code = 401, message = "not authorized!"), 
+	            @ApiResponse(code = 403, message = "forbidden!!!"),
+	            @ApiResponse(code = 404, message = "not found!!!") })
 	@GetMapping("/contactos")
 	public ResponseEntity<List<Contacto>> findAll() {
 		return null;
 
 	}
 
+	 @ApiOperation(value = "Obtiene un contacto ", response = Iterable.class, tags = "getContact")
+	    @ApiResponses(value = { 
+	            @ApiResponse(code = 200, message = "Suceess|OK"),
+	            @ApiResponse(code = 401, message = "not authorized!"), 
+	            @ApiResponse(code = 403, message = "forbidden!!!"),
+	            @ApiResponse(code = 404, message = "not found!!!") })
 	@GetMapping("/contacts/{contactId}")
 	public ResponseEntity<Contacto> findContactById() {
 
@@ -29,6 +45,12 @@ public class ContactoController {
 
 	}
 
+	 @ApiOperation(value = "Agrega un nuevo contacto ", response = Iterable.class, tags = "newContact")
+	    @ApiResponses(value = { 
+	            @ApiResponse(code = 200, message = "Suceess|OK"),
+	            @ApiResponse(code = 401, message = "not authorized!"), 
+	            @ApiResponse(code = 403, message = "forbidden!!!"),
+	            @ApiResponse(code = 404, message = "not found!!!") })
 	@PostMapping("/contacto")
 	public ResponseEntity<Contacto> addContact() {
 
@@ -36,12 +58,24 @@ public class ContactoController {
 
 	}
 
+	 @ApiOperation(value = "Actualiza un  contacto ", response = Iterable.class, tags = "updateContact")
+	    @ApiResponses(value = { 
+	            @ApiResponse(code = 200, message = "Suceess|OK"),
+	            @ApiResponse(code = 401, message = "not authorized!"), 
+	            @ApiResponse(code = 403, message = "forbidden!!!"),
+	            @ApiResponse(code = 404, message = "not found!!!") })
 	@PutMapping("/contacts/{contactId}")
 	public ResponseEntity<Contacto> updateContact() {
 		return null;
 
 	}
 
+	 @ApiOperation(value = "Elimina un  contacto ", response = Iterable.class, tags = "deleteContact")
+	    @ApiResponses(value = { 
+	            @ApiResponse(code = 200, message = "Suceess|OK"),
+	            @ApiResponse(code = 401, message = "not authorized!"), 
+	            @ApiResponse(code = 403, message = "forbidden!!!"),
+	            @ApiResponse(code = 404, message = "not found!!!") })
 	@DeleteMapping(path = "/contacts/{contactId}")
 	public ResponseEntity<Void> deleteContactById() {
 
